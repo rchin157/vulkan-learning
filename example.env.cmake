8559089ed7d38725bf10781cf256ed0898743622
Rylan Chin <rchin@ualberta.ca>
2023-02-28 15:13:11 -0700

    example cmake env
    since this file changes depending on computer, remove it and just leave an example

--- example.env.cmake
+++ example.env.cmake
@@ -3,7 +3,7 @@ set(GLM_PATH C:/dev/Libraries/glm)
 set(VULKAN_SDK_PATH C:/VulkanSDK/1.3.239.0)
 
 # Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
-set(MINGW_PATH "C:/Program Files/mingw64")
+set(MINGW_PATH "C:/dev/x86_64-12.2.0-release-win32-seh-msvcrt-rt_v10-rev2/mingw64")
 
 # Optional set TINYOBJ_PATH to target specific version, otherwise defaults to external/tinyobjloader
 # set(TINYOBJ_PATH X:/dev/Libraries/tinyobjloader)
